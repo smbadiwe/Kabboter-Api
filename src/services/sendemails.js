00154ad9158b1f3e6d7d5be730bd97f5848d67fb
smbadiwe <somasystemsng@gmail.com>
2018-07-11 10:12:34 -0700

    Added back .env.example; made few changes to email sending codes

--- src/services/sendemails.js
+++ src/services/sendemails.js
@@ -1,13 +1,34 @@
 import Emailer from "./Emailer";
+import log from "../utils/log";
 
 export async function sendWelcomeEmailToPlayer(user) {
-  const viewData = { title: "Welcome to Kabooter", user: user };
+  if (!user || !user.email) {
+    log.debug("sendWelcomeEmailToPlayer: No user or user email NoDataReceived. So, we're aborting");
+    return;
+  }
+  const viewData = {
+    title: `Welcome to ${process.env.APP_NAME}`,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    username: user.username
+  };
   const options = { to: user.email, subject: viewData.title };
   return await new Emailer().sendEmail(options, "welcome-player.html", viewData);
 }
 
 export async function sendWelcomeEmailToModerator(user) {
-  const viewData = { title: "Welcome to Kabooter", user: user };
+  if (!user || !user.email) {
+    log.debug(
+      "sendWelcomeEmailToModerator: No user or user email NoDataReceived. So, we're aborting"
+    );
+    return;
+  }
+  const viewData = {
+    title: `Welcome to ${process.env.APP_NAME}`,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    username: user.username
+  };
   const options = { to: user.email, subject: viewData.title };
   return await new Emailer().sendEmail(options, "welcome-moderator.html", viewData);
 }
