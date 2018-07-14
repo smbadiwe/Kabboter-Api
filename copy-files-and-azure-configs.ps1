9b09e03ab21231754ed64a815766decb97df07d5
smbadiwe <somasystemsng@gmail.com>
2018-07-14 05:40:18 -0700

    Fixed Babel transpile issue. Refactored a bit. Made app fit for Azure

--- copy-files-and-azure-configs.ps1
+++ copy-files-and-azure-configs.ps1
@@ -1,8 +1,6 @@
 echo "Copying the UI files for game plays"
-copy ./src/public/quizadmin/quizadmin.html ./dist/public/quizadmin/quizadmin.html
-copy ./src/public/surveyadmin/surveyadmin.html ./dist/public/surveyadmin/surveyadmin.html
-copy ./src/public/quizplayer/quizplayer.html ./dist/public/quizplayer/quizplayer.html
-copy ./src/public/surveyplayer/surveyplayer.html ./dist/public/surveyplayer/surveyplayer.html
+Copy-Item -Path ./src/public -Recurse -Destination ./dist/public/ -Container
+
 
 echo "Copying config files for Azure"
 
--- useful-links.md
+++ useful-links.md
@@ -3,3 +3,7 @@
 - https://lowendbox.com/blog/getting-started-with-mysql-over-ssl/ (Most useful)
 - https://www.a2hosting.com/kb/cpanel/cpanel-database-features/remote-mysql-access
 - https://www.digitalocean.com/community/tutorials/how-to-configure-ssl-tls-for-mysql-on-ubuntu-16-04
+
+## Running the app on A2Hosting
+
+- https://www.a2hosting.com/kb/installable-applications/manual-installations/installing-node-js-on-managed-hosting-accounts#Installation-prerequisites
