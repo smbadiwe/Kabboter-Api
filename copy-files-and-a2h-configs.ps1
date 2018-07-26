b45478a3f325a6ae168d7ae902c60f386cbdbf5c
smbadiwe <somasystemsng@gmail.com>
2018-07-26 02:10:33 -0700

    Changes before going live on A2h (with Pusher)

--- copy-files-and-a2h-configs.ps1
+++ copy-files-and-a2h-configs.ps1
@@ -1,8 +1,9 @@
 echo "Copying the UI files for game plays"
-copy ./src/public/quizadmin/quizadmin.html ./dist/public/quizadmin/quizadmin.html
-copy ./src/public/surveyadmin/surveyadmin.html ./dist/public/surveyadmin/surveyadmin.html
-copy ./src/public/quizplayer/quizplayer.html ./dist/public/quizplayer/quizplayer.html
-copy ./src/public/surveyplayer/surveyplayer.html ./dist/public/surveyplayer/surveyplayer.html
+Copy-Item -Path ./src/public -Recurse -Destination ./dist/public/ -Container
+# copy ./src/public/quizadmin/quizadmin.html ./dist/public/quizadmin/quizadmin.html
+# copy ./src/public/surveyadmin/surveyadmin.html ./dist/public/surveyadmin/surveyadmin.html
+# copy ./src/public/quizplayer/quizplayer.html ./dist/public/quizplayer/quizplayer.html
+# copy ./src/public/surveyplayer/surveyplayer.html ./dist/public/surveyplayer/surveyplayer.html
 
 echo "Copying config files for Azure"
 
